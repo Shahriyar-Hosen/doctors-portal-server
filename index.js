708944f5443994f8b2932e98af38ec25b5a53f55
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-05-11 20:41:49 +0600

    servicesCollection add done

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 // Middleware
@@ -8,18 +10,27 @@ app.use(cors());
 app.use(express.json());
 // ---------------------------
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7toku.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
 // async await function
 async function run() {
-    
-    // try catch finally
-    try {
-        
-    } finally {
-        
-    }
+  // try catch finally
+  try {
+    await client.connect();
+    const servicesCollection = client
+      .db("doctors-portal")
+      .collection("services");
+  } finally {
+  }
 }
 // call function catch (console dir)
-run().catch(console.dir)
+run().catch(console.dir);
 // --------------------------------------------
 
 // http://localhost:5000/
