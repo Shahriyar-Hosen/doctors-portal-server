805788f88d522bc49640a9e2741d319dee28bee7
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-05-15 19:56:54 +0600

    error solved await miccing

--- index.js
+++ index.js
@@ -133,7 +133,7 @@ async function run() {
     // Get Admin user search by email
     app.get("/admin/:email", async (req, res) => {
       const email = req.params.email;
-      const user = userCollection.findOne({ email: email });
+      const user = await userCollection.findOne({ email: email });
       const isAdmin = user.role === "admin";
       res.send({ admin: isAdmin });
     });
