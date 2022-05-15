6fbb19c31facb9a745a2b42f29e4e864c22345fe
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-05-15 18:39:42 +0600

    pdate (upsert / insert) user admin data in db

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ async function run() {
     });
     // -------------------------------------------
     // Get  api to read all users
-    app.get("/user", async (req, res) => {
+    app.get("/user", verifyJWT, async (req, res) => {
       const users = await userCollection.find().toArray();
       res.send(users);
     });
@@ -130,6 +130,18 @@ async function run() {
     });
     // --------------------------------------------------------
 
+    //  Update (upsert / insert) user admin data in db
+    app.put("/user/admin/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      const filter = { email: email };
+      const updateDoc = {
+        $set: { role: "admin" },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+    // -------------------------------------------
+
     //  Update (upsert / insert) user data in db
     app.put("/user/:email", async (req, res) => {
       const email = req.params.email;
@@ -141,7 +153,7 @@ async function run() {
       };
       const result = await userCollection.updateOne(filter, updateDoc, options);
       var token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: "1h",
+        expiresIn: "1d",
       });
       res.send({ result, token });
     });
